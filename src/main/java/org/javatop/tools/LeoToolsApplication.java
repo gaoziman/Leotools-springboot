97253d1f63e3731ecfe0ef92977f338c464e3b4f
gaoziman <2942894660@qq.com>
2024-01-24 21:25:29 +0800

    添加基本依赖信息

---
+++ src/main/java/org/javatop/tools/LeoToolsApplication.java
@@ -0,0 +1,25 @@
+package org.javatop.tools;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+/**
+ * @author : Leo
+ * @version 1.0
+ * @date 2024-01-24 20:07
+ * @description : 启动类
+ */
+@SpringBootApplication
+public class LeoToolsApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(LeoToolsApplication.class, args);
+        Environment environment = context.getBean(Environment.class);
+
+        System.out.println("访问链接：http://localhost:" + environment.getProperty("server.port"));
+        System.out.println("(♥◠‿◠)ﾉﾞ  项目启动成功 ლ(´ڡ`ლ)ﾞ \n");
+    }
+}
