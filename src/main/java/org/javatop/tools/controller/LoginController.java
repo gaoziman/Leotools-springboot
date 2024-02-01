38731e58cf782a1f217a452d9aea1ff2aca038ae
gaoziman <2942894660@qq.com>
2024-02-01 15:31:46 +0800

    修改登录接口

--- src/main/java/org/javatop/tools/controller/LoginController.java
+++ src/main/java/org/javatop/tools/controller/LoginController.java
@@ -1,19 +1,22 @@
 package org.javatop.tools.controller;
 
+import org.javatop.tools.common.result.ResultCodeEnum;
+import org.javatop.tools.exception.BusinessException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author : Leo
  * @version 1.0
  * @date 2024-01-29 11:48
- * @description :
+ * @description : 登录接口
  */
 @RestController
 public class LoginController {
 
-    @GetMapping("/login")
-    public String login() {
+    @GetMapping("/login/{id}")
+    public String login(@PathVariable Integer id) {
         return "login success";
     }
 }
